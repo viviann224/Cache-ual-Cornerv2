7f8713219e5473d558ec0c40e6af3dc311144570
Will <wmccrory@gmail.com>
2018-03-18 11:27:21 -0500

    Don't remember what I changed *shrug*

--- public/js/skin.js
+++ public/js/skin.js
@@ -2,13 +2,13 @@ var skin = localStorage.getItem("skinChoice");
 
 switch (skin) {
 	case "girly":
-		$("head").append('<link rel="stylesheet" href="/stylesheets/skins/dennis/girly.css" type="text/css" />');
+		$("head").append('<link rel="stylesheet" href="/stylesheets/skins/dennis/girlboss.css" type="text/css" />');
 		break;
 	case "christmas":
-		$("head").append('<link rel="stylesheet" href="/stylesheets/skins/dennis/christmas.css" type="text/css" />');
+		$("head").append('<link rel="stylesheet" href="/stylesheets/skins/dennis/xmas.css" type="text/css" />');
 		break;
 	case "urban":
-		$("head").append('<link rel="stylesheet" href="/stylesheets/skins/dennis/urban.css" type="text/css" />');
+		$("head").append('<link rel="stylesheet" href="/stylesheets/skins/dennis/brickhouse.css" type="text/css" />');
 		break;
 	case "msdos":
 		$("head").append('<link rel="stylesheet" href="/stylesheets/skins/will/msdos.css" type="text/css" />');
